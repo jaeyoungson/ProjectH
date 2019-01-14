7c78b18c3964c1ce7c10725d827736337ce6b37e
seungjae <seungjae0327@nate.com>
2019-01-15 02:16:49 +0900

    Temp_Monster
    REDcolor Temp_Monster

---
+++ ProjectH/Assets/Temp_Monster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp_Monster : MonoBehaviour
+{
+    private Vector3 target;
+    private float dampSpeed = 1.0f;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * dampSpeed);
+    }
+}
